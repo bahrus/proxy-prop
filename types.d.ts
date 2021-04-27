d31ecfa7e7803407a24a5f10972c5f0672cb0c74
bahrus <anderson.bruce.b@gmail.com>
2021-04-27 17:59:14 -0400

    ...

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,6 @@
-import {PDFrom} from 'pass-down/types.d.js';
-export interface IProxyPropProps extends PDFrom {
+import {PDToFrom} from 'pass-down/types.js';
+export interface IProxyPropProps extends PDToFrom {
     fromRootNodeHost?: boolean,
-    to?: string,
-    careOf?: string,
-    prop?: string,
-    as?: 'str-attr' | 'bool-attr' | 'obj-attr',
-    lastVal: any,
     observeProp?: string,
     hostToObserve?: Element,
     m?: number,
